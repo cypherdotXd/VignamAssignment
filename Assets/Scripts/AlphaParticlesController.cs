190ef3758587269ada97d2b049f11be6fdd80b03
Aashish Patel <64499848+cypherdotXd@users.noreply.github.com>
2024-03-03 17:03:35 +0530

    init

---
+++ Assets/Scripts/AlphaParticlesController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaParticlesController : MonoBehaviour
+{
+    [SerializeField] ParticleSystem alphaParticles;
+
+    private void Awake()
+    {
+        ToggleTrace(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetAlphaParticlesEnergy(float value)
+    {
+        ParticleSystem.MainModule main = alphaParticles.main;
+        main.startSpeed = value;
+    }
+
+    public void ToggleTrace(bool value)
+    {
+        ParticleSystem.TrailModule trails = alphaParticles.trails;
+        trails.enabled = value;
+    }
+}
---
+++ Assets/Scripts/Atom.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atom : MonoBehaviour
+{
+    [SerializeField] private int protons = 2;
+    [SerializeField] private int neutrons = 2;
+    [SerializeField] private ParticleSystem neutronsParticles;
+    [SerializeField] private ParticleSystem protonsParticles;
+    [SerializeField] private ParticleSystemForceField positiveForceField;
+
+    private void Awake()
+    {
+        // 
+        SetProtonCount(protons);
+        SetNeutronCount(neutrons);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetProtonCount(float count)
+    {
+        neutronsParticles.Clear();
+        neutronsParticles.Emit((int)count);
+        ParticleSystem.ShapeModule shape = neutronsParticles.shape;
+        shape.radius = count / 30;
+        positiveForceField.endRange = count / 30 + 0.5f;
+    }
+
+    public void SetNeutronCount(float count)
+    {
+        protonsParticles.Clear();
+        protonsParticles.Emit((int)count);
+        ParticleSystem.ShapeModule shape = protonsParticles.shape;
+        shape.radius = count / 30;
+        positiveForceField.endRange = count / 30 + 0.5f;
+    }
+}
